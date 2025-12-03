8de57333ca6a7e8bb653564f63368c04c5dcf1f8
Saien Govender <saiengvd@gmail.com>
2025-12-03 13:28:25 +0200

    Splash Activity

--- app/src/main/java/com/example/mmcoffee/Activity/MainActivity.kt
+++ app/src/main/java/com/example/mmcoffee/Activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.mmcoffee
+package com.example.mmcoffee.Activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mmcoffee.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/mmcoffee/Activity/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mmcoffee.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mmcoffee.R
+import com.example.mmcoffee.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+    }
+}
